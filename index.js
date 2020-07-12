79e4820c43be61932bccb7eae61677f1abf39f7e
evansx1 <douglasleal.15@gmail.com>
2020-07-12 18:14:39 -0300

    add api configuration

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const locationRoutes = require("./routes/location");
 
+const cors = require("cors");
 const app = express();
+
+//API configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
 
 app.use('/v1', locationRoutes);
 
